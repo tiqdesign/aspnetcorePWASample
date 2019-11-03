d3ebf62a4eb8f52c4e7a2b0f0adceb63a3737952
Tarık KAYA <tiqqq24@gmail.com>
2019-11-03 23:20:50 +0300

    PWA sample w1

---
+++ tiqpwa/Controllers/AnasayfaController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tiqpwa.Controllers
+{
+    public class AnasayfaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DevamEdenListe()
+        {
+            return View();
+        }
+
+        public IActionResult BeklemeListe()
+        {
+            return View();
+        }
+        public IActionResult TamamlananListe()
+        {
+            return View();
+        }
+        public IActionResult IptalOlanListe()
+        {
+            return View();
+        }
+
+        public IActionResult Takvim()
+        {
+            return View();
+        }
+    }
+}
---
+++ tiqpwa/Controllers/GirisController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using tiqpwa.Models;
+
+namespace tiqpwa.Controllers
+{
+    public class GirisController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Kullanici k) {
+            if (ModelState.IsValid)
+            {
+                HttpContext.Session.SetString("Kullanıcı_Adı",k.KullaniciAdi.ToUpper());
+                return RedirectToAction("Index", "Anasayfa");
+            }
+            else
+                return View();
+        }
+    }
+}
